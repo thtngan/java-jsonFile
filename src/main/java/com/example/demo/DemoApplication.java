dd803a35ae27407a2210dc0c7ddeeafee6563e2b
thtngan <thtngan>
2021-11-23 18:41:35 +0700

    Save inputStream to class and print list

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,40 +1,40 @@
 package com.example.demo;
 
 
-import org.springframework.asm.TypeReference;
-import org.springframework.boot.SpringApplication;
+import com.example.demo.models.Employee;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
 import java.util.List;
 
-import com.example.demo.models.Employee;
-
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) throws IOException {
-//		SpringApplication.run(DemoApplication.class, args);
-
+	public static void main(String[] args) throws IOException  {
 		String path = "../demo/src/main/resources/EmployeeList.json";
-		File initialFile = new File(path);
-		InputStream fileInputStream = new FileInputStream(initialFile);
+		List<Employee> listEmployee = readEmployee(path);
+		writeEmployee(listEmployee);
 
-		InputStreamReader inputStream = new InputStreamReader(fileInputStream);
-		//Creating a BufferedReader object
-		BufferedReader input = new BufferedReader(inputStream);
-
-
-		StringBuffer sb = new StringBuffer();
-		String str;
-		while((str = input.readLine())!= null){
-			sb.append(str + "\n");
-		}
-		System.out.println(sb.toString());
 	}
 
+	private static List<Employee> readEmployee(String pathName) throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
 
+		File file = new File(pathName);
+		InputStream inputStream = new FileInputStream(file);
+		TypeReference<List<Employee>> typeReference = new TypeReference<List<Employee>>() {
+		};
+		List<Employee> listEmployee = objectMapper.readValue(inputStream, typeReference);
 
+		return listEmployee;
+	}
+
+	private static void writeEmployee(List<Employee> list) {
+		list.stream()
+				.forEach(employee -> System.out.println(employee.toString()));
+	}
 
 
 }
--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -1,11 +1,33 @@
 package com.example.demo.models;
-
 public class Employee {
     private String name;
     private int age;
 
+    //default constructor
+    public Employee()
+    {
+        super();
+    }
+
     public Employee(String name, int age) {
         this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
